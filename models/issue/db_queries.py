3267830ca95739a066aa5c94fe1341880f5bac03
Dani-Obando <daobandona@est.utn.ac.cr>
2025-08-01 01:46:30 -0600

    endpoint fixed

--- models/issue/db_queries.py
+++ models/issue/db_queries.py
@@ -1,4 +1,26 @@
-from db.mongo_client import Connection
+from pymongo import MongoClient
 from decouple import config
+from bson import ObjectId
 
-__dbmanager__ = Connection(config('ISSUE_COLLECTION'))
+# Conexión a MongoDB
+client = MongoClient(config('MONGO_URL'))
+db = client[config('MONGO_DB')]
+__dbmanager__ = db[config('ISSUE_COLLECTION')]
+
+def insert_data(data):
+    return __dbmanager__.insert_one(data)
+
+def get_all_data():
+    return __dbmanager__.find()
+
+def find_one(query):
+    return __dbmanager__.find_one(query)
+
+def delete_data(object_id):
+    return __dbmanager__.delete_one({"_id": object_id})
+
+def update_data(issue_id, data):
+    return __dbmanager__.update_one({"_id": ObjectId(issue_id)}, {"$set": data})
+
+def get_by_id(lab_book_id):
+    return __dbmanager__.find_one({"_id": ObjectId(lab_book_id)})
